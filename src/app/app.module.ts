700895ed21a83ede8c5b04a77766289b6f2cca80
Francisco Javier Hernandez Angeles <farjav_rnr@hotmail.com>
2017-01-18 12:11:35 -0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { routes, navigatableComponents } from "./app.routes";
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: navigatableComponents,
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { RouterModule } from '@angular/router';
+
+import { AppComponent }  from './app.component';
+
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+
+import { MainComponent } from "./pages/main/main.component";
+
+export const routes = [
+    { path: "", component: MainComponent },
+];
+
+export const navigatableComponents = [
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderComponent,
+];
